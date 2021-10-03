cf10170857604ff07928954ca3ae465a9eb47560
adamhoof <adam.hofman1@gmail.com>
2021-10-03 10:36:20 +0200

    remove unpacker

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	_ "github.com/lib/pq"
+	"strings"
 )
 
 func main() {
@@ -11,31 +13,20 @@ func main() {
 	postgreHandler.CreateTable()
 	postgreHandler.ImportFromCSV()
 	postgreHandler.Disconnect()
-	/*apiHandler := APIHandler{}
-	unpacker := Unpacker{}
+
 	barcodeController := BarcodeController{}
 	serialHandler := SerialHandler{}
 	serialHandler.PortConfig("/dev/ttyAMA0", 9600)
 	serialHandler.OpenPort()
-	barcodeController.CreateBarcode(serialHandler.port)
+	barcodeController.CreateBarcodeReader(serialHandler.port)
 
 	for {
 		barcodeOutput, err := barcodeController.barcode.ReadBytes('\x0d')
 		if err != nil {
 			panic(err)
 		}
-		fmt.Print("\033[H\033[2J")
+		fmt.Println(barcodeOutput)
 		stringBarcodeOutput := string(barcodeOutput)
 		stringBarcodeOutput = strings.Replace(stringBarcodeOutput, "\r", "", -1)
-		rawProductData := apiHandler.RequestProductData(stringBarcodeOutput)
-		productData := unpacker.UnpackJSON(rawProductData)
-
-		fmt.Println()
-		fmt.Println("Cena/Price:")
-		fmt.Print(productData["price"])
-		fmt.Println(",-")
-		fmt.Println()
-		fmt.Print("Jednotková cena/")
-		fmt.Println("Unit Price:")
-		fmt.Println(productData["price_in_sale"])*/
+	}
 }
