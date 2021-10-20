51f6174469ba04aa303e957d2a4c5fb2462efea0
adamhoof <adam.hofman1@gmail.com>
2021-10-20 19:49:35 +0200

    add space

--- main.go
+++ main.go
@@ -44,7 +44,7 @@ func main() {
 		formatter.PrintColoredText(actualPriceColor, "\n"+"\n"+"\n"+"\n"+"\n"+
 			"Cena za kus: "+
 			stringPrice+" Kč"+
-			"\n"+"\n"+"\n"+"\n"+"\n")
+			"\n"+"\n"+"\n"+"\n"+"\n"+"\n")
 
 		formatter.PrintColoredText(unitOfMessureColor, "Přepočet na měrnouj. ("+mj+"): "+
 			stringPricePerMj+" Kč")
