bf6acc4fcb5144c13bdb5fa21badc2b94de1d0f7
adamhoof <adam.homfna1@gmail.com>
2022-06-27 15:39:38 +0200

    rewrite main to comply with previous changes

--- cmd/main.go
+++ cmd/main.go
@@ -3,33 +3,37 @@ package main
 import (
 	"MedunkaOpBarcode/pkg/Barcode"
 	"MedunkaOpBarcode/pkg/Database"
+	"MedunkaOpBarcode/pkg/Formatting"
 	"MedunkaOpBarcode/pkg/SerialCommunication"
-	"MedunkaOpBarcode/pkg/TextFormatting"
 	"bufio"
+	"database/sql"
 	"fmt"
 	_ "github.com/lib/pq"
-	"gopkg.in/gookit/color.v1"
+	"os"
 	"strconv"
 	"strings"
 )
 
-var actualPriceStyle = color.Style{color.FgRed, color.OpBold}
-var defaultStyle = color.Style{color.FgLightWhite, color.OpItalic}
-
 func main() {
-	postgreHandler := Database.PostgreSQLHandler{}
-	formatter := TextFormatting.Formatter{}
+	dbPort, err := strconv.Atoi(os.Getenv("dbPort"))
+	if err != nil {
+		panic(err)
+	}
+	dbConfig := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s",
+		os.Getenv("host"), dbPort, os.Getenv("user"), os.Getenv("password"), os.Getenv("dbname"))
 
-	postgreHandler.Connect()
-	postgreHandler.DropTableIfExists()
-	postgreHandler.CreateTable()
-	postgreHandler.ImportFromCSV()
+	var db *sql.DB
+	Database.Connect(db, dbConfig)
+	Database.ExecuteStatement(db, Database.DropExistingTableSQL)
+	Database.ExecuteStatement(db, Database.CreateTableSQL)
+	Database.ExecuteStatement(db, Database.ImportFromCSVToTableSQL)
 
 	portConfig := SerialCommunication.CreatePortConfig("/dev/ttyAMA0", 9600)
-	port := SerialCommunication.OpenPort(portConfig)
+	serialPort := SerialCommunication.OpenPort(portConfig)
 
 	var reader *bufio.Reader
-	Barcode.AssignPort(reader, port)
+	Barcode.AssignPort(reader, serialPort)
 
 	for {
 		barcodeAsByteArray := Barcode.Read(reader)
@@ -37,24 +41,24 @@ func main() {
 
 		barcodeAsByteArray = barcodeAsByteArray[:len(barcodeAsByteArray)-1]
 		barcodeAsInt, _ := strconv.ParseInt(string(barcodeAsByteArray), 10, 64)
-		barcodeAsString := formatter.ToString(barcodeAsInt)
+		barcodeAsString := Formatting.ToString(barcodeAsInt)
 
-		name, stock, price, mj, mjkoef := postgreHandler.QueryProductData(barcodeAsString)
+		name, stock, price, mj, mjkoef := Database.QueryProductData(db, barcodeAsString)
 
 		formatedPrice := strings.ReplaceAll(price, ".00 Kč", "")
-		stringPricePerMj := formatter.ToString(formatter.ToFloat(formatedPrice) * mjkoef)
+		stringPricePerMj := Formatting.ToString(Formatting.ToFloat(formatedPrice) * mjkoef)
 
-		formatter.PrintColoredText(defaultStyle, name)
+		Formatting.PrintStyledText(Formatting.DefaultStyle, name)
 		fmt.Println("")
 		fmt.Println("")
-		formatter.PrintColoredText(actualPriceStyle,
+		Formatting.PrintStyledText(Formatting.ActualPriceStyle,
 			"Cena za ks: "+
 				formatedPrice+"Kč"+
 				"\n"+"\n")
 
-		formatter.PrintColoredText(defaultStyle, "Přepočet na měrnouj. ("+mj+"): "+
+		Formatting.PrintStyledText(Formatting.DefaultStyle, "Přepočet na měrnouj. ("+mj+"): "+
 			stringPricePerMj+"Kč")
-		formatter.PrintColoredText(defaultStyle, "\n")
-		formatter.PrintColoredText(defaultStyle, "Stock: "+stock)
+		Formatting.PrintStyledText(Formatting.DefaultStyle, "\n")
+		Formatting.PrintStyledText(Formatting.DefaultStyle, "Stock: "+stock)
 	}
 }
